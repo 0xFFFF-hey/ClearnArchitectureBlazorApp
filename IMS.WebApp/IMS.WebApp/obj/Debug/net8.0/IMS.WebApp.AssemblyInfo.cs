f0af5dd61215fdf3ae7655923f41d566f36ba74e
helyousfi <visioncomputer200@gmail.com>
2025-02-04 21:52:51 +0100

    Home page

--- IMS.WebApp/IMS.WebApp/obj/Debug/net8.0/IMS.WebApp.AssemblyInfo.cs
+++ IMS.WebApp/IMS.WebApp/obj/Debug/net8.0/IMS.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0943b9865b5aae4b5952173c20c185f25862b5e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b5edf3526c8867a487ebf96631d469464e93c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
